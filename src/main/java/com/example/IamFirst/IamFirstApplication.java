59bc23de9726699ffb31cb8c824532ffb1e1d336
TilekKulanbekov <kulanbekov519@gmail.com>
2022-02-03 15:29:07 +0600

    push

---
+++ src/main/java/com/example/IamFirst/IamFirstApplication.java
@@ -0,0 +1,22 @@
+package com.example.IamFirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class IamFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IamFirstApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String index() {
+		return "<h1>Hello Tilek!</h1>";
+	}
+
+}
+
---
+++ src/test/java/com/example/IamFirst/IamFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.IamFirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IamFirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
